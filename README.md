37d7d5f7078774ec8eac4bcebe478df2844bf57d
Ayhan Akilli <info@ayhan.biz>
2019-12-25 14:54:50 +0100

    Add demo

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Custom CKEditor 5 classic build
 
 ## Demo
 
-https://akilli.github.io/rte/ck5/build-classic.html
+https://akilli.github.io/ckeditor5-build-classic/demo
---
+++ demo/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+(function (document, ClassicEditor) {
+    document.addEventListener('DOMContentLoaded', () => {
+        ClassicEditor
+            .create(document.querySelector('#rte'), {
+                media: {
+                    browser: '/demo-browser/media.html'
+                }
+            })
+            .then(editor => document.getElementById('save').addEventListener('click', () => console.log(editor.getData())))
+            .catch(error => console.error(error));
+    });
+})(document, ClassicEditor);
